d3b346d0cf3c784df9fc8e904abcd75885c15f6f
Mariano Peterson <mariano@petersonpages.com>
2012-07-11 10:38:26 -0700

    Imlemented MVCC transactional style data access/manipulation for StorageEngine methods: get,set,unset,numequalto

--- server.py
+++ server.py
@@ -18,18 +18,20 @@ BUFFER = 1024
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s')
 log = logging.getLogger(__name__)
 
-''' Python synchronizes Dicts and Lists (i.e., they are thread-safe),
-    UNLESS __eq__ or __hash__ have been overridden. For the purpose
-    of this demo, we'll assume these methods have NOT been overridden.
+"""
+Python synchronizes Dicts and Lists (i.e., they are thread-safe),
+UNLESS __eq__ or __hash__ have been overridden. For the purpose
+of this demo, we'll assume these methods have NOT been overridden.
 
-    TODO: explicitly synchronize access to the database, by using threading.Lock.
-'''
+TODO: explicitly synchronize access to the database, by using threading.Lock.
+"""
 
 class MemDbServer:
-    """ Server: Simply delegates client connections to QueryEngine threads.
+    """
+    Server: Simply delegates client connections to QueryEngine threads.
 
-                The client-server communication protocol is implemented by QueryEngine.
-                The storage tasks are implemented by StorageEngine.
+    The client-server communication protocol is implemented by QueryEngine.
+    The storage tasks are implemented by StorageEngine.
     """
 
     def __init__(self, host, port, conns):
@@ -63,10 +65,11 @@ class MemDbServer:
 
 
 class QueryEngine:
-    """ QueryEngine implements the client-server communication protocol.
+    """
+    QueryEngine implements the client-server communication protocol.
 
-        Connection handling is implemented by the MemDbServer.
-        Storage is implemented by the StorageEngine.
+    Connection handling is implemented by the MemDbServer.
+    Storage is implemented by the StorageEngine.
     """
 
     def __init__(self, storage):
@@ -76,29 +79,61 @@ class QueryEngine:
 
     def start(self, socket, address):
         log.debug('connected to: {}:{}'.format(address[0], address[1]))
+        tx = None
+
         while True:
             data = socket.recv(BUFFER)
             if not data:
                 break
+
             try:
+                if not tx:
+                    tx = self.storage.get_transaction()
+
                 log.debug("received: {}".format(data))
                 command, args = self.parse(data)
+
+                # TODO: handle transaction commands: begin, commit, rollback
+                if command == 'begin':
+                    log.debug('BEGIN TRANSACTION')
+                    socket.send('[TRANSACTION] OK')
+                    continue
+                elif command == 'rollback':
+                    log.debug('ROLLBACK TRANSACTION')
+                    socket.send('OK')
+                    continue
+                elif command == 'commit':
+                    log.debug('COMMIT TRANSACTION')
+                    socket.send('OK')
+                    continue
+
                 engine = getattr(self.storage, command)
-                result = engine(*args)
+                result = engine(tx, *args)
                 reply = 'OK' if result is None else str(result)
                 socket.send(reply)
+
             except QueryError as e:
                 msg = "ERROR: {}".format(e.value)
-                log.error(e)
+                log.error(msg)
                 socket.send(msg)
+
+            except BaseException as e:
+                msg = str(e)
+                log.error(msg)
+                socket.send(msg)
+
+            except:
+                socket.send("UNKNOWN ERROR")
 
         socket.close()
         log.debug('disconnected from {}:{}'.format(address[0], address[1]))
 
     def parse(self, query):
-        """ Parses query and returns (methodToCall, [args]).
-            MethodToCall: reference to a StorageEngine method,
-            Args:         list of args to pass to the StorageEngine method.
+        """
+        Parses query, and returns (methodToCall, [args]).
+
+        MethodToCall: reference to a StorageEngine method,
+        Args:         list of args to pass to the StorageEngine method.
         """
         commands = {
             # command   : (methodToCall, argCount)
@@ -106,7 +141,10 @@ class QueryEngine:
             'GET'       : ('get'        , 1),
             'UNSET'     : ('unset'      , 1),
             'NUMEQUALTO': ('numequalto' , 1),
-            'END'       : ('end'        , 0)
+            'END'       : ('end'        , 0),
+            'BEGIN'     : ('begin'      , 0),
+            'ROLLBACK'  : ('rollback'   , 0),
+            'COMMIT'    : ('commit'     , 0)
         }
 
         words = query.split()
@@ -127,41 +165,99 @@ class QueryError(Exception):
         self.value = value
 
 
+class Transaction:
+
+    def __init__(self, id, active_transactions):
+        self.id = 0
+        self.active_transactions = []
+
+
 class StorageEngine:
     db = {}
+    index = {}
+    tx = {}
 
     def __init__(self):
-        #pass
         self.db = {} # key: value
         self.index = {} # value: [keys]
+        self.tx = {
+            'next': 0,
+            'last_commit': 0,
+            'active_transactions': []
+        }
 
-    def set(self, key, value):
-        if key in self.db and self.index[self.db[key]]:
-            # Remove key from index for this value
-            del self.index[self.db[key]][key]
-        self.db[key] = value
+    def get_transaction(self):
+        """
+        todo: make this thread-safe, using threading.lock()
+        """
+        tx_id = self.tx['next']
+        self.tx['next'] += 1
+        self.tx['active_transactions'].append(tx_id)
+        return Transaction(tx_id, self.tx['active_transactions'])
+
+    def set(self, tx, key, value):
+        # Don't remove keys from the index. Instead, rely on the read
+        # methods to validate each key referenced by the index.
+        # TODO: create a garbage collector to compact the index
+
+        # Add tuple to this row
+        row_tuple = {'xmin': tx.id, 'xmax': None, 'value': value}
+        if key in self.db:
+            self.db[key].append(row_tuple)
+        else:
+            self.db[key] = [row_tuple]
+
+        # Add this key to index for this value
         if value in self.index:
             self.index[value][key] = True
         else:
             self.index[value] = {key: True}
 
-    def get(self, key):
+    def get(self, tx, key):
+        """
+        Rules to determine if a tuple is visible:
+        Creation transaction id (xmin) is...
+          - a committed transaction
+          - less than the current transaction's id (tx.id)
+          - not in an active transaction when the current transaction began
+
+        AND, the expiry transaction id (xmax) is...
+          - blank, or refs an aborted transaction
+          - greater than the current transaction id
+          - in an active transaction when the current transaction began
+        """
         if key in self.db:
-            return self.db[key]
+            for row in self.db.get(key):
+                xmin, xmax = row['xmin'], row['xmax']
+                if (xmin in self.db['tx_log']) and (self.db['tx_log'][xmin] == 'committed') and \
+                        xmin < tx.id and \
+                        xmin not in tx.active_transactions and \
+                        (xmax is None or \
+                            (xmax in self.db['tx_log'] and self.db['tx_log'][xmax] == 'aborted') or \
+                            xmax > tx.id or \
+                            xmax in tx.active_transactions):
+                    return row['value']
         return 'NULL'
 
-    def unset(self, key):
+    def unset(self, tx, key):
         if key in self.db:
-            if self.db[key] in self.index:
-                # Remove key from index for this value
-                del self.index[self.db[key]][key]
-            del self.db[key]
+            # Don't remove keys from the index. Instead, rely on the read
+            # methods to validate each key referenced by the index.
+            # TODO: create a garbage collector to compact the index
 
-    def numequalto(self, value):
+            # Add a tuple to this row, logging the expiration of the row.
+            # TODO: rather than add another tuple, we should just find the current tuple and expire it.
+            row_tuple = {'xmin': tx.id, 'xmax': tx.id, 'value': value}
+            if key in self.db:
+                self.db[key].append(row_tuple)
+
+    def numequalto(self, tx, value):
+        count = 0
         if value in self.index:
-            # return ", ".join(map(str, self.index[value].keys()))
-            return len(self.index[value])
-        return 0
+            for key in self.index:
+                if self.get(tx, key) != 'NULL':
+                    count += 1
+        return count
 
 if __name__ == '__main__':
     server = MemDbServer(HOST, PORT, CONNS)
